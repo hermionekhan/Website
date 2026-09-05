82208a1b0f5d57ea25f357b6df988f14e93e9357
Hermione Khan <hermione.khan1@gmail.com>
2026-09-05 14:15:19 +0100

    05.09.26 1

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-    const gifElement = document.getElementById('interactive-gif');
+    //const gifElement = document.getElementById('interactive-gif');
 
     // Define GIF source URLs
-    const hoverGif = 'CSS/Assets/thesix_in.gif';
-    const unhoverGif = 'CSS/Assets/thesix_out.gif';
+    //const hoverGif = 'CSS/Assets/thesix_in.gif';
+    //const unhoverGif = 'CSS/Assets/thesix_out.gif';
 
-    new Image().src = hoverGif;
+    //new Image().src = hoverGif;
 
     // Play hover GIF from frame 1
-    gifElement.addEventListener('mouseenter', () => {
-      gifElement.src = `${hoverGif}?t=${Date.now()}`;
-      });
+    //gifElement.addEventListener('mouseenter', () => {
+      //gifElement.src = `${hoverGif}?t=${Date.now()}`;
+      //});
 
     // Play unhover GIF from frame 1
-    gifElement.addEventListener('mouseleave', () => {
-      gifElement.src = `${unhoverGif}?t=${Date.now()}`;
-    });
+    //gifElement.addEventListener('mouseleave', () => {
+      //gifElement.src = `${unhoverGif}?t=${Date.now()}`;
+    //});
 
 
 
